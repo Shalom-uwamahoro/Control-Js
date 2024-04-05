ecee512526fce3e42ab0a5ba063362f8ce130b4e
Shalom-uwamahoro <shalomuwamahoro@gmail.com>
2024-04-05 18:56:54 +0300

    added answers

---
+++ control.js
@@ -0,0 +1,71 @@
+//Write a program that takes in an array of numbers and consoles the first four items multiplied by itself  and the last two added by 10. Return the array with the new values
+
+var arrNumb= [1,2,3,4,5,6,7,8, 9, 10 ,11,12];
+
+function multiplyAdd(arrNumb){
+    arrNumb.forEach((numb, index)=>{
+        if(index < 4){
+            arrNumb[index] = numb * numb;
+        }
+        else if(index >= arrNumb.length - 2){
+            arrNumb[index]= numb + 10;
+        }
+
+ })
+    console.log(arrNumb);
+    return arrNumb;
+   
+     };
+multiplyAdd(arrNumb);
+
+//Write a program that takes in the following array and use a while loop to iterate and break when the item is equal to the fourth index :let arrNum = [1,2,3,4,5,6,7,8,9];
+
+let arrNum = [1,2,3,4,5,6,7,8,9];
+let i = 0;
+     while (i < arrNum.length)
+      {  if (arrNum[i] === 3)
+        break; 
+         console.log(arrNum[i])
+         i++;
+       };
+
+
+//Write a function that takes in a an array of strings and use a continue statement when the item is at the second index:  let fruits= ['apple','plum','banana','strawberries','kiwi']
+
+let fruits= ['apple','plum','banana','strawberries','kiwi']
+
+function arrStrs(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (i === 1) 
+            continue; 
+            console.log(array[i])
+        } };
+
+  arrStrs(fruits);
+
+
+// //Write a function that accepts an array of strings and console.logs each element using a for loop.
+
+   function arrString(arr)
+   { for (let j = 0; j < arr.length; j++) {
+       console.log(arr[j]); }
+   }
+    arrString(['Nice', 'Digestive', 'Riham', 'Akarabo']);
+
+
+// //Write a JavaScript function that takes a string as input and reverses it using a while loop. The function should return the reversed string.
+
+
+function reversedString(strs){
+  let reverse = '';
+  let j = strs.length - 1;
+     while (j >= 0)
+     { reverse += strs[j]; j--; }
+    return reverse; };
+
+let reverseString = reversedString("I enjoy coding");   
+ console.log(reverseString);
+
+
+
+
